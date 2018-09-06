8576f574bc44f268f232e1b01ba05af050a6a98e
César Loo <cesarlooe@gmail.com>
2018-09-06 06:20:18 -0500

    Integración inicial de Google Auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { UsuarioService } from '../services/service.index';
 import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -16,14 +17,36 @@ export class LoginComponent implements OnInit {
   email: string;
   recuerdame = false;
 
+  auth2: any;
+
   constructor(private router: Router, private _usuarioService: UsuarioService) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
     this.email = localStorage.getItem('email') || '';
     if (this.email.length > 0) {
       this.recuerdame = true;
     }
+  }
+
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '263846650669-6tod11q9vln4j38covapt9pmcgas2n9q.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('btnGoogle'));
+    });
+  }
+
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      // const profile = googleUser.getBasicProfile();
+      const token = googleUser.getAuthResponse().id_token;
+      console.log(token);
+    });
   }
 
   ingresar(forma: NgForm) {
